4821483bf9a35bc3184c9a64367ef188e2b4bcff
Agustin Saunders <saundersagustin94@gmail.com>
2023-02-04 01:25:25 -0300

    primer commit y Lector

---
+++ src/main/java/com/agustines/lector/LectorDeFacturasApplication.java
@@ -0,0 +1,13 @@
+package com.agustines.lector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectorDeFacturasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectorDeFacturasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agustines/lector/LectorDeFacturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustines.lector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectorDeFacturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
