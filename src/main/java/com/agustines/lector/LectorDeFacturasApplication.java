7d6a80fe924d54d41a2ee33ec2e30a68a7ea1dc9
Agustin Saunders <saundersagustin94@gmail.com>
2023-02-04 21:08:30 -0300

    tercer commit, algo anda

--- src/main/java/com/agustines/lector/LectorDeFacturasApplication.java
+++ src/main/java/com/agustines/lector/LectorDeFacturasApplication.java
@@ -1,32 +1,27 @@
 package com.agustines.lector;
-
-import com.itextpdf.text.pdf.PdfReader;
-import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import java.io.File;
 import java.io.IOException;
-
 @SpringBootApplication
 public class LectorDeFacturasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LectorDeFacturasApplication.class, args);
+		File muestra = new File("E:/Proyects/Lector de faturas/lector-de-facturas/facturas/prueba3.png");
+		ITesseract imagen = new Tesseract();
+		try {
+			String resultado = imagen.doOCR(muestra);
+			String cae = "CAE N";
+			int indice = resultado.indexOf(cae);
+			String listo = resultado.substring(indice, indice + 21);
+			System.out.println(listo);
 
-		try{
-			String ubicacion = "E:/Proyects/Lector de faturas/lector-de-facturas/facturas/factura1.pdf";
-			PdfReader factura = new PdfReader(ubicacion);
-
-			int paginas= factura.getNumberOfPages();
-			for (int i=1; i<=paginas; i++){
-				String content= PdfTextExtractor.getTextFromPage(factura, i);
-				System.out.println(content);
-			}
-
-
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} ;
+		}catch (TesseractException e){
+			System.out.println("Exception " + e.getMessage());
+		}
 	}
-
 }
